c7f2cba158790d6334025c140701960f5c1f3927
wangchengkai <me@chengkai.wang>
2018-11-22 10:32:52 +0800

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## vue-ssr
+
+> vue同构项目初探，参照 https://ssr.vuejs.org
--- app/server.js
+++ app/server.js
@@ -6,24 +6,8 @@ const renderer = require('vue-server-renderer').createBundleRenderer(path.resolv
   template: require('fs').readFileSync('./app/template/index.template.html', 'utf-8')
 });
 
-// const createApp = require('../src/app');
-
 server.get('*', (req, res) => {
   const context = {url: req.url, title: '同构测试'};
-  // createApp(context).then(app => {
-  //   renderer.renderToString(app, (err, html) => {
-  //     if (err) {
-  //       if (err.code === 404) {
-  //         res.status(404).end('Page not found')
-  //       } else {
-  //         res.status(500).end('服务器内部错误');
-  //       }
-  //     } else {
-  //       res.end(html);
-  //     }
-  //   });
-  // });
-
   renderer.renderToString(context, (err, html) => {
     if (err) {
       console.error(err);
