ef31cf4a611a5b51a42605e8bd8bce9b7c999e15
wangchengkai <me@chengkai.wang>
2018-11-23 17:26:23 +0800

    doc

--- README.md
+++
@@ -1,7 +0,0 @@
-## vue-ssr
-
-vue同构项目初探，参照官方介绍
-
-> https://ssr.vuejs.org
-
-> https://ssr.vuejs.org/api/
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-<div>
+<div id="app">
   <p>Vue 同构测试</p>
   <hr />
+  <table>
+    <tr>
+      <td>Hi</td>
+    </tr>
+  </table>
   <router-view></router-view>
 </div>
 
